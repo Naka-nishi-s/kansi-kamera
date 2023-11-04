56b30484946937d487aa90b228fe1b079b7f484e
Nakanishi <ponzxp@gmail.com>
2023-11-05 00:23:12 +0900

    WIP

--- kanshiKamera/kanshiKamera/settings.py
+++ kanshiKamera/kanshiKamera/settings.py
@@ -40,24 +40,20 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'watch_room',
     'nice',
-    'corsheaders',
+    # 'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    # 'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'kanshiKamera.urls'
-
-# dev Only
-CORS_ALLOW_ALL_ORIGINS = True
 
 TEMPLATES = [
     {
