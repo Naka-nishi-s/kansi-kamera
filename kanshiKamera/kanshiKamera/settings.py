187d602c6799c32bf8230f6340dbe70632774807
Nakanishi <ponzxp@gmail.com>
2023-11-05 16:56:33 +0900

    WIP

--- kanshiKamera/kanshiKamera/settings.py
+++ kanshiKamera/kanshiKamera/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-*nv5r!#76xc5j)06qjs@j9q7=c+ekdm*%)1uv8^cur!37a&vv$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.11.15', 'localhost', '127.0.0.1', 'nakanishi-room.com']
 
 
 # Application definition
